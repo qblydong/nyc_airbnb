46bcb33b92307083a4781d5a219c48112835869f
Lingyi <lingyi_dong@mckinsey.com>
2023-03-06 11:35:40 -0700

    add docstrings and parameter types

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,9 +60,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-def test_row_count(data):
+def test_row_count(data: pd.DataFrame):
+    """
+    Test if the number of rows in our data falls into our desired range.
+    """
     assert 15000 < data.shape[0] < 1000000
 
 
-def test_price_range(data, min_price, max_price):
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Test if prices in our data all fall into our desired user-defined range.
+    """
     assert data["price"].between(min_price, max_price).all()
